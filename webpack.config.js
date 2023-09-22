0f83f92b835fc6c00d5418005cec606194e29885
Evgeniy Shvetsov <e.shvetsov@runetsoft.ru>
2023-09-22 21:15:57 +0700

    basic files

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,33 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name]-[hash].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.(eot|svg|ttf|otf|woff|woff2)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   },
@@ -21,11 +41,19 @@ module.exports = {
     path: distPath,
     filename: 'index_bundle.js'
   },
+  resolve: {
+    alias: {
+      styles: path.resolve(__dirname, './src/assets/styles'),
+      images: path.resolve(__dirname, './src/assets/images'),
+    },
+  },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
   ],
   devServer: {
     port: 9009,
-    open: true
+    //open: true
   }
 }

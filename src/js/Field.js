97ae2182c6e6298232f944f855ab0a43bababfa9
Evgeniy Shvetsov <e.shvetsov@runetsoft.ru>
2023-09-23 13:18:08 +0700

    just fill field

--- src/js/Field.js
+++ src/js/Field.js
@@ -4,7 +4,17 @@ import blocks from './blocks'
 export class Field {
   constructor(canvas) {
     this.context = canvas.getContext('2d')
+
+    // config
+    this.numberRows = 8
+    this.numberColumns = 8
+    this.width = 540
+    this.height = 600
+
+    // computed
     this.images = []
+    this.tileWidth = this.width / this.numberColumns
+    this.tileHeight = this.height / this.numberRows
   }
 
   init() {
@@ -21,7 +31,18 @@ export class Field {
   }
 
   fill() {
-    const tile = new Tile(this.context, this.images[0])
-    tile.draw()
+    for (let col = 0; col < this.numberColumns; col++) {
+      for (let row = 0; row < this.numberRows; row++) {
+        let x = col * this.tileWidth
+        let y = row * this.tileHeight
+        const tile = new Tile(this.context, x, y, this.tileWidth, this.tileHeight, this.getRandomImage())
+        tile.draw()
+      }
+    }
+  }
+
+  getRandomImage() {
+    const random = Math.floor(Math.random() * this.images.length)
+    return this.images[random]
   }
 }
--- src/js/Tile.js
+++ src/js/Tile.js
@@ -1,16 +1,16 @@
 export class Tile {
-  constructor(context, image) {
+  constructor(context, x, y, width, height, image) {
     this.context = context
     this.image = image
 
-    this.width = 60
-    this.height = 70
-    this.x = 0
-    this.y = 0
-
+    this.width = width
+    this.height = height
+    this.startPosition = {
+      x, y
+    }
   }
 
   draw() {
-    this.context.drawImage(this.image, this.x, this.y, this.x + this.width, this.y + this.height)
+    this.context.drawImage(this.image, this.startPosition.x, this.startPosition.y, this.width, this.height)
   }
 }
